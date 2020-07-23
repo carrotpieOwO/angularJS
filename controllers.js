bbab93ee7432782bab49d7d79dbfe0794f9e5fbf
hayong <xxhayoxx@naver.com>
2020-07-24 01:52:39 +0900

    apply추가

--- controllers.js
+++ controllers.js
@@ -3,31 +3,31 @@ app.controller("TodoCtrl", function ($scope, todoStorage, $anchorScroll) {
   $scope.today = new Date().toLocaleDateString();
   $scope.loadingState = false;
   $scope.todos;
+  $scope.percentage;
 
   //다시보기
   //todo 데이터 받아오기
-  (async function getTodos(){
-    await todoStorage.get().then((res)=>{
-      
-      $scope.percentage = function(res){
-        let completedCount = 0;
-        let todosLength = res.length;
-        let percentage = 0;
-
-        if(todosLength != 0){
-          angular.forEach(res, function(todo){
-            if(todo.completed){completedCount++}
-          })
-          percentage = Math.ceil(completedCount/todosLength *100);
-        }
-        console.trace('digest trace')
-        console.time('digest time')
-        return percentage;
-      }
-
-      $scope.$apply(function(){
+  (async function getTodos() {
+    console.log('호출됨')
+    await todoStorage.get().then((res) => {
+      $scope.$apply(function () {
         $scope.todos = res;
-        $scope.loadingState =true;
+        $scope.loadingState = true;
+        $scope.percentage = function (todos) {
+          let completedCount = 0;
+          let todosLength = todos.length;
+          let percentage = 0;
+
+          if (todosLength != 0) {
+            angular.forEach(todos, function (todo) {
+              if (todo.completed) { completedCount++ }
+            })
+            percentage = Math.ceil(completedCount / todosLength * 100);
+          }
+          // console.trace('digest trace')
+          // console.time('digest time')
+          return percentage;
+        }
       });
     })
   })()
@@ -35,42 +35,42 @@ app.controller("TodoCtrl", function ($scope, todoStorage, $anchorScroll) {
   //투두 추가
   $scope.formData = {};
   $scope.add = function (newTodoCategory, newTodoTitle, newTodoDate, newColor) {
-     // console.log(newTodoCategory, newTodoDate, newTodoTitle, newColor);
-      todoStorage.add(newTodoCategory, newTodoTitle, newTodoDate, newColor);
+    // console.log(newTodoCategory, newTodoDate, newTodoTitle, newColor);
+    todoStorage.add(newTodoCategory, newTodoTitle, newTodoDate, newColor);
 
     //add 하고나서 폼 비워주기
-      $scope.formData.newTodoTitle = '';  
-      $scope.formData.newTodoCategory = '';
-      $scope.formData.newTodoDate = '';
-      $scope.formData.newColor = '';
+    $scope.formData.newTodoTitle = '';
+    $scope.formData.newTodoCategory = '';
+    $scope.formData.newTodoDate = '';
+    $scope.formData.newColor = '';
   };
 
   //투두 수정
   $scope.update = function () {
     todoStorage.update();
   };
-  
+
   //투두 삭제
   $scope.remove = function (todo) {
     todoStorage.remove(todo);
   };
 
   //날짜이동
-  $scope.changeDate = function(indicator){
+  $scope.changeDate = function (indicator) {
     const nextDay = new Date($scope.today);
     const prevDay = new Date($scope.today);
-    nextDay.setDate(nextDay.getDate()+1);
-    prevDay.setDate(prevDay.getDate()-1);
-    if(indicator === 'next'){
+    nextDay.setDate(nextDay.getDate() + 1);
+    prevDay.setDate(prevDay.getDate() - 1);
+    if (indicator === 'next') {
       $scope.today = nextDay.toLocaleDateString();
       //console.log($scope.today)
-    }else{
+    } else {
       $scope.today = prevDay.toLocaleDateString();
     }
   }
 
   //투두 필터: 마감일 지난거 보여주지 않기
-  $scope.hideClosingTodos = function(todo){
+  $scope.hideClosingTodos = function (todo) {
     return todo.closingDate >= $scope.today
   }
 
--- services.js
+++ services.js
@@ -1,14 +1,14 @@
 app.factory('todoStorage', function () {
-    const TODO_DATA = 'TODO_DATA'; 
+    const TODO_DATA = 'TODO_DATA';
     const storage = {
         //todos 데이터
         todos: [],
 
         //service 내에서 외부에서 감춰줘야 하는 경우 (컨트롤러에서 사용할 수 없음, 오직 서비스내에서만 사용)
         _saveToLocalStorage: function (data) {
-            localStorage.setItem(TODO_DATA, JSON.stringify(data)) 
+            localStorage.setItem(TODO_DATA, JSON.stringify(data))
         },
-        
+
         _getFromLocalStorage: function () {
             //데이터가 없을 경우에 빈문자열 리턴
             return JSON.parse(localStorage.getItem(TODO_DATA)) || []
@@ -16,17 +16,17 @@ app.factory('todoStorage', function () {
 
         //데이터 조작 함수
         get: function () {
-            return new Promise((resolve)=>{
+            return new Promise((resolve) => {
                 //storage.todos = storage._getFromLocalStorage();
-                angular.copy(storage._getFromLocalStorage(), storage.todos);         
-                setTimeout(()=>{
-                    resolve( storage.todos)
-                },3000)
+                angular.copy(storage._getFromLocalStorage(), storage.todos);
+                setTimeout(() => {
+                    resolve(storage.todos)
+                }, 3000)
             })
         },
 
         remove: function (todo) {
-            var idx = storage.todos.findIndex( item => item.title === todo.title);
+            var idx = storage.todos.findIndex(item => item.title === todo.title);
 
             if (idx > -1) {
                 storage.todos.splice(idx, 1)
@@ -35,8 +35,8 @@ app.factory('todoStorage', function () {
         },
 
         add: function (newTodoCategory, newTodoTitle, newTodoDate, newColor) {
-        
-            if(newTodoCategory === '' || newTodoCategory === undefined){
+
+            if (newTodoCategory === '' || newTodoCategory === undefined) {
                 newTodoCategory = '기타'
             }
 
@@ -46,10 +46,10 @@ app.factory('todoStorage', function () {
                 newColor = storage.todos[idx].color;
             }
 
-            if(newTodoDate === '' || newTodoDate === undefined ){
+            if (newTodoDate === '' || newTodoDate === undefined) {
                 newTodoDate = new Date().toLocaleDateString();
             }
- 
+
             console.log(newTodoCategory, newTodoDate, newTodoTitle, newColor);
             //create new todos
             var newTodo = {
